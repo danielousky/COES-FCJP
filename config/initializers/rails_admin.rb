3af152fef83fe92f230aee25254bca01be6e5d63
Daniel Moros <danielousky@gmail.com>
2025-07-22 19:36:07 -0400

    Se eliminó el modelo `Course` de la lista de excepciones en la configuración de eliminación de Rails Admin, simplificando así la gestión de datos en la interfaz de administración.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -142,7 +142,7 @@ RailsAdmin.config do |config|
     end
 
     delete do
-      except [School, StudyPlan, Faculty, EnrollAcademicProcess, Course]
+      except [School, StudyPlan, Faculty, EnrollAcademicProcess]
     end
 
     import do
