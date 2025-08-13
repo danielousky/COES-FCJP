2191b15dae366cbee837d123614ea2ac033b725e
Daniel Moros <danielousky@gmail.com>
2025-08-13 13:24:24 -0400

    Se modificó el controlador `ValidarController` para cambiar el layout a 'visitor' y se mejoró la lógica de validación en el método `constancias`. Además, se actualizaron las vistas para mostrar mensajes de alerta y un enlace para volver al inicio, optimizando la experiencia del usuario al validar documentos.

--- app/controllers/validar_controller.rb
+++ app/controllers/validar_controller.rb
@@ -2,10 +2,11 @@ class ValidarController < ApplicationController
   before_action :set_version, only: %i[ constancias ]
   skip_before_action :authenticate_user!, only: [ :constancias ]
 
-  layout 'logged'
+  layout 'visitor'
   def constancias
-
-    if (@version and @version.item.is_a? EnrollAcademicProcess and (params[:study] and @version.event.eql? 'Se generó Constancia de Estudio') or (@version.event.eql? 'Se generó Constancia de Inscripción'))
+    @title = 'Validador de Documento'
+    @valido = @version&.item.is_a?(EnrollAcademicProcess) and (params[:study] and @version&.event.eql? 'Se generó Constancia de Estudio') or (@version&.event.eql? 'Se generó Constancia de Inscripción')
+    if (@valido)
 
       @study = params[:study] ? true : false
 
@@ -14,7 +15,6 @@ class ValidarController < ApplicationController
       
     else
       flash[:danger] = 'Recurso no accesible. Puede que el documento no sea válido o halla sido alterado. Contacte a las autoridades para la validación del documento.'
-      redirect_to root_path
     end
 
   end
@@ -37,7 +37,7 @@ class ValidarController < ApplicationController
         
       rescue Exception => e
         flash[:danger] = "Recurso no accesible. Puede que el documento no sea válido o halla sido alterado. Contacte a las autoridades para la validación del documento: #{e}"
-        redirect_to root_path
+
       end
     end
 
